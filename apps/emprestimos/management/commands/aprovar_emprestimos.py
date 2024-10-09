32fd550bab289a6cb23743d593021c3675afa8a6
Caio Wendhausem Kormives <cwkormives@gmail.com>
2024-10-08 23:06:01 -0300

    fix: adjust code legibility and change orm querys to requests on api

--- apps/emprestimos/management/commands/aprovar_emprestimos.py
+++ apps/emprestimos/management/commands/aprovar_emprestimos.py
@@ -1,13 +1,28 @@
 from django.core.management.base import BaseCommand
-from apps.emprestimos.models import Emprestimo
+import requests
 
 class Command(BaseCommand):
+    BASE_URL = 'http://127.0.0.1:8080/api/v1'
 
     def handle(self, *args, **kwargs):
-        emprestimos = Emprestimo.objects.filter(taxa_juros__gt=4, aprovado=False) # para aprovação
+        try:
+            # Fetching all loans
+            emprestimos_response = requests.get(f'{self.BASE_URL}/emprestimos/')
+            emprestimos_response.raise_for_status()
+            emprestimos = emprestimos_response.json()
 
-        for emprestimo in emprestimos:
-            emprestimo.aprovado = True
-            emprestimo.save()
+            for emprestimo in emprestimos:
+                if emprestimo['taxa_juros'] > 4 and not emprestimo['aprovado']:
+                    emprestimo_id = emprestimo['id']
+                    emprestimo['aprovado'] = True
 
-        self.stdout.write(self.style.SUCCESS(f'{emprestimos.count()} empréstimos aprovados.'))
+                    # Updating the loan approval status
+                    update_response = requests.put(f'{self.BASE_URL}/emprestimos/{emprestimo_id}/', json=emprestimo)
+                    update_response.raise_for_status()
+
+                    self.stdout.write(self.style.SUCCESS(f'Empréstimo {emprestimo_id} aprovado.'))
+
+        except requests.RequestException as e:
+            self.stdout.write(self.style.ERROR(f'Erro ao processar empréstimos: {e}'))
+
+        self.stdout.write(self.style.SUCCESS('Processo de aprovação de empréstimos concluído!'))
